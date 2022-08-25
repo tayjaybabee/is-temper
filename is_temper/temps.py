924ebdfb196883ee36b18e02d285cfc90e5e3767
Taylor-Jayde Blackstone <tayjaybabee@gmail.com>
2022-08-24 23:12:39 -0600

    Add more code.

---
+++ is_temper/temps.py
@@ -0,0 +1,21 @@
+"""
+
+File:
+    is_temper/temps.py
+    
+Project:
+    is-temper
+    
+Created:
+    8/24/22 - 17:43 hrs
+    
+Author:
+    Taylor-Jayde Blackstone <t.blackstone@inspyre.tech>
+    
+Description:
+    Contains code related to the gathering of CPU temperatures.
+    
+"""
+
+
+class CPU(object):
